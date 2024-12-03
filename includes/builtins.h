73b8c6943e88dbb5eaec7dc8bc0039447a5049de
Aaron Padilla <aapadill@student.hive.fi>
2024-12-03 23:31:17 +0200

    missed free_array prototype

--- includes/builtins.h
+++ includes/builtins.h
@@ -30,6 +30,7 @@ void	spush(t_sstack *stack, t_snode *node);
 t_snode	*spop(t_sstack *stack);
 t_snode	*sinit_node(char *value);
 t_sstack	*sinit_stack(void);
+void	free_str_array(char **str);
 
 char	**ft_split(char const *s, char c, int *n);
 int		ft_strcmp(const char *s1, const char *s2);
